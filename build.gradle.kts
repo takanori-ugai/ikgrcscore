c6462391db98661969535a2800817e79cb93dfd7
Takanori Ugai <ugai@fujitsu.com>
2025-03-11 12:20:07 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,10 +28,10 @@ repositories {
 dependencies {
 //    implementation(kotlin("stdlib"))
     implementation("io.javalin:javalin:6.5.0")
-    kapt("io.javalin.community.openapi:openapi-annotation-processor:6.5.0-1")
+    kapt("io.javalin.community.openapi:openapi-annotation-processor:6.5.0")
     implementation("io.javalin.community.openapi:javalin-openapi-plugin:6.5.0") // for /openapi route with JSON scheme
-    implementation("io.javalin.community.openapi:javalin-swagger-plugin:6.5.0-1") // for Swagger UI
-    implementation("io.javalin.community.openapi:javalin-redoc-plugin:6.5.0-1") // for ReDoc UI
+    implementation("io.javalin.community.openapi:javalin-swagger-plugin:6.5.0") // for Swagger UI
+    implementation("io.javalin.community.openapi:javalin-redoc-plugin:6.5.0") // for ReDoc UI
     implementation("io.javalin:javalin-rendering:6.5.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
